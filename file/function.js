6e78bfcd6682977497dc0259dabd48bc7dbf8c82
Damzi09 <156364503+Zkhack2@users.noreply.github.com>
2026-06-25 00:56:53 +0000

    remove clock and message-box

--- file/function.js
+++ file/function.js
@@ -35,28 +35,3 @@ $(window).resize(function() {
 	};
 })(jQuery);
 
-function timeElapse(date){
-	var current = new Date();
-	var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
-	var days = Math.floor(seconds / (3600 * 24));
-	seconds = seconds % (3600 * 24);
-	var hours = Math.floor(seconds / 3600);
-	if (hours < 10) {
-		hours = "0" + hours;
-	}
-	seconds = seconds % 3600;
-	var minutes = Math.floor(seconds / 60);
-	if (minutes < 10) {
-		minutes = "0" + minutes;
-	}
-	seconds = seconds % 60;
-	if (seconds < 10) {
-		seconds = "0" + seconds;
-	}
-	var result = "Jours <span class=\"digit\">" + days + "</span> Heures <span class=\"digit\">" + hours + "</span> Minutes <span class=\"digit\">" + minutes + "</span> Secondes <span class=\"digit\">" + seconds;
-	$("#clock").html(result);
-
-	var text = "LE MONDE A PLUS DE CHANCE DEPUIS ";
-	$("#message-box").html(text);
-
-}
