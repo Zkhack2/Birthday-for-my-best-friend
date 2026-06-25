90c70634cbfde026a65ead4d2240c2d7efcf2682
Damzi09 <156364503+Zkhack2@users.noreply.github.com>
2026-06-25 00:39:33 +0000

    fix: update texts for best friend (not girlfriend)

--- file/love.js
+++ file/love.js
@@ -168,8 +168,8 @@
             ctx.moveTo(0, 0);
             ctx.scale(0.75, 0.75);
             ctx.font = "12px Verdana";
-            ctx.fillText("Click Me:) ", 30, -5);
-            ctx.fillText("Birthday Queen !", 28, 10);
+            ctx.fillText("Click Me :)", 30, -5);
+            ctx.fillText("Bestie Forever !", 28, 10);
             ctx.restore();
         },
         clear: function() {
