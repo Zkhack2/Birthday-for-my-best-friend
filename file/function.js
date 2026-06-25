3fda91d9151ef7ad9313ce941efdad5f7b72cfeb
Damzi09 <156364503+Zkhack2@users.noreply.github.com>
2026-06-25 01:01:28 +0000

    fix: make scaling dynamic on resize, remove unused vars

--- file/function.js
+++ file/function.js
@@ -1,18 +1,5 @@
 
 
-// variables
-var $win = $(window);
-var clientWidth = $win.width();
-var clientHeight = $win.height();
-
-$(window).resize(function() {
-    var newWidth = $win.width();
-    var newHeight = $win.height();
-    if (newWidth != clientWidth && newHeight != clientHeight) {
-        location.replace(location);
-    }
-});
-
 (function($) {
 	$.fn.typewriter = function() {
 		this.each(function() {
